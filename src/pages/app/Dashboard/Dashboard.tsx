1289838deb5bc483cfc8221593d764384ae799d3
Sergio Souz2 <sergio.tbl0123@gmail.com>
2025-04-24 00:45:47 -0300

    feat: cards dashboard

--- src/pages/app/Dashboard/Dashboard.tsx
+++ src/pages/app/Dashboard/Dashboard.tsx
@@ -1,8 +1,19 @@
+import { CardDayOrdersAmount } from './components/CardDayOrdersAmount/CardDayOrdersAmount'
+import { CardMonthCanceledOrdersAmount } from './components/CardMonthCanceledOrdersAmount/CardMonthCanceledOrdersAmount'
+import { CardMonthOrdersAmount } from './components/CardMonthOrdersAmount/CardMonthOrdersAmount'
+import { CardMonthRecenue } from './components/CardMonthRecenue/CardMonthRecenue'
+
 export function Dashboard() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
-      <div className="grid grid-cols-4 gap-4"></div>
+
+      <div className="grid grid-cols-4 gap-6">
+        <CardMonthRecenue />
+        <CardMonthOrdersAmount />
+        <CardDayOrdersAmount />
+        <CardMonthCanceledOrdersAmount />
+      </div>
     </div>
   )
 }
